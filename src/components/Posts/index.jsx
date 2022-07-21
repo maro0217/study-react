52092b63c766fe58f6696ec39f9cd090f7ece339
Maro Yoshi <ydaye07@gmail.com>
2022-07-21 13:58:24 +0900

    🎨 Replaced to SWR.

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,76 +1,25 @@
-import { useCallback, useEffect, useReducer, useState } from 'react'
+import { usePosts } from 'src/hooks/usePosts'
 
-const initialState = {
-  data: [],
-  loading: true,
-  error: null
-};
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "end" :
-      return  {
-        ...state,
-        data: action.data,
-        loading: false,
-      };
-    case "error" :
-      return  {
-        ...state,
-        error,
-        loading: false,
-    };
-    default:
-      throw new Error("no such action type!");
-  }
-}
 
 export const Posts = (props) => {
-
-  // const [posts, setPosts] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
-const [state, dispatch] =  useReducer(reducer, initialState)
-
-  const getPosts = useCallback(async () => {
-    try {
-
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if(!res.ok) {
-        throw new Error("データの取得に失敗しました")
-      }
-      const json = await res.json();
-      dispatch({type: "end", data: "json"})
-
-      // setPosts(json)
-    } catch (error) {
-      dispatch({type: "error", error})
-    } finally {
-      // setLoading(false);
-
-    }
-  }, [])
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
-  if(state.loading) {
+  const {data, error, isLoading, isEmpty} = usePosts();
+  if(isLoading) {
     return <div>ローディング中です</div>
   }
-  if(state.error) {
-    return <div>{state.error.message}</div>
+  if(error) {
+    return <div>{error.message}</div>
   }
-  if(state.data.length === 0) {
+  if(isEmpty) {
     return <div>データは空です</div>
   }
 
-
+  console.log(data);
   return (
         <ol>
-            {state.data.map((post) => {
+            {data.map((post) => {
             return <li key={post.id}>{post.title}</li>
-            }) }
+            }) } 
         </ol>
     )
 }
