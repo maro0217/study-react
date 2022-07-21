50d9e9b844e4f8f309f6519955b31bca53457bf2
Maro Yoshi <ydaye07@gmail.com>
2022-07-21 16:18:39 +0900

    🔥 不必要なコードやファイルを削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,17 +1,11 @@
 import 'src/styles/globals.css'
 import Head from "next/head"
-import { useBgColor } from 'src/hooks/useBgColor'
-import { useCounter } from 'src/hooks/useCounter'
-import { useInputArray } from 'src/hooks/useInputArray'
 
 
 
 //引数の分割代入
 const MyApp = ({ Component, pageProps }) => {
-  const counter = useCounter();
-  const  inputArray = useInputArray();
 
-  useBgColor();
 
   return (
       <>
@@ -19,7 +13,7 @@ const MyApp = ({ Component, pageProps }) => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         {/* pagesの親みたいなもの。propsを渡したら各ページにpropsが渡される */}
-        <Component {...pageProps} {...counter} {...inputArray}/>
+        <Component {...pageProps}/>
       </>
       )
 }
