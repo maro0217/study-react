25931054710edd8b1677b4d2842bd471a7996f3f
Maro Yoshi <ydaye07@gmail.com>
2022-07-21 14:23:23 +0900

    🚚 ファイルシステムルーティング！！

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { usePosts } from 'src/hooks/usePosts'
 
 
@@ -18,7 +19,12 @@ export const Posts = (props) => {
   return (
         <ol>
             {data.map((post) => {
-            return <li key={post.id}>{post.title}</li>
+            return (
+              <li key={post.id}>
+                <Link href={`/post/${post.id}`}>
+                  {post.title}
+                </Link>
+              </li>)
             }) } 
         </ol>
     )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { useCallback, useEffect, useState } from 'react'
 import { Header } from 'src/components/Header'
 import { Posts } from 'src/components/Posts'
 import styles from 'src/styles/Home.module.css'
