68d527f7d10357cac7518f34c5e94ad79eec66b8
Maro Yoshi <ydaye07@gmail.com>
2022-07-21 16:24:13 +0900

    🔥 いらないもの削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
-import { Main } from 'src/components/Main'
 import styles from 'src/styles/Home.module.css'
 
 const Index = () => {
