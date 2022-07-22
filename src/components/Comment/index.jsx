6707d6bacd703bda710ca7c43d7ae477fafa8880
Maro Yoshi <ydaye07@gmail.com>
2022-07-22 11:30:47 +0900

    ✨ コメントページに投稿情報を追加

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
-import { useComment } from 'src/hooks/useComment';
+import { PostByPostId } from 'src/components/Post/PostByPostId';
+import { useComment } from 'src/hooks/useFetchData';
 
 
 export const Comment = () => {
-  const {comment, user, error, isLoading} = useComment();
+  const {data, error, isLoading} = useComment();
 
   if(isLoading) {
     return <div>ローディング中です</div>
@@ -15,10 +16,13 @@ export const Comment = () => {
   return (
         <div>
             <Head>
-                <title>{comment?.id}</title>
+                <title>{data?.id}</title>
             </Head>
-            <h1>{comment?.body}</h1>
-            {comment.body ? (<p>Created by {user.name}</p>) : null}
+            <h1>{data?.body}</h1>
+            {data.body ? (<p>Created by {data.name}</p>) : null}
+            <h3>Original Feed</h3>
+            <PostByPostId id={data.postId} />
+
         </div>
     )
 }
---
+++ src/components/Post/PostByPostId.jsx
@@ -0,0 +1,22 @@
+import { fetcher } from "src/utils/fetcher"
+import useSWR from "swr"
+
+export const PostByPostId = (props) => {
+    const {data, error} =  useSWR(
+        props.id
+         ? `https://jsonplaceholder.typicode.com/posts/${props.id}`
+         : null,
+        fetcher)
+
+        if(!data && !error) {
+            return <div>ローディング中です</div>
+          }
+
+        if(error) {
+            return <div>{error.message}</div>
+          }
+    
+    return (
+        <div>{data.body}</div>
+    )
+}
