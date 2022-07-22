f2ffe186814460ca353fa44cc3bc7f4fccc77aef
Maro Yoshi <ydaye07@gmail.com>
2022-07-22 13:22:36 +0900

    💄 既存のスタイルをTailwindに書き換え

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,16 @@
-import styles from 'src/components/Layout/Layout.module.css'
 
 export const Layout = (props) => {
     return (
-        <div className={styles.container}>
+        <div className="flex flex-col items-center mx-auto max-w-2xl px-2 min-h-screen">
             {props.children}
         </div>
         )
 }
+
+//     min-height: 100vh;
+//   padding: 0 0.5rem;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   max-width: 80%;
+//   margin: 0 auto;
